47d626f58557107adbd0b3ac29d4e0096e8f253c
nelvi <nelvicas97@gmail.com>
2026-07-05 13:21:24 -0600

    implementacion de controllers.vistas,rutas

---
+++ app/Views/productos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Catálogo de Productos</h1>
+    <p>"Esta es mi primera vista en CodeIgniter."</p>
+    
+</body>
+</html>
